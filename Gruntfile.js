8084b9b35c2d3f9be30a2a474c0fbbc26447f037
marianagalm <contatogalm@gmail.com>
2025-06-24 19:54:31 -0300

    build

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,6 +28,5 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production']);
 }
 
