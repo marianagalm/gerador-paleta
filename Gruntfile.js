87bf6efced378b5dfbc6b9bbe366f7662d9efe7c
marianagalm <contatogalm@gmail.com>
2025-06-24 19:41:02 -0300

    htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,6 +22,37 @@ module.exports = function (grunt) {
                 tasks: ['less:development']
             }
         },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.hmtl'
+                }
+            }
+        },
         uglify: {
             target: {
                 files: {
@@ -34,6 +65,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('build', ['less:production', 'uglify']);
